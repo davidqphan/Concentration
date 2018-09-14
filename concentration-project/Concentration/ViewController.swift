e77f1f7f015a94491582528e1b5465ebfecc5e86
David Phan <david.phan@artamata.com>
2018-09-14 14:49:13 -0700

    moved demo project into own folder

---
+++ concentration-project/Concentration/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by David Phan on 9/10/18.
+//  Copyright © 2018 David Phan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count + 1) / 2
+    }
+    
+    private func updateFlipCountLabel() {
+        let attributes = [NSAttributedStringKey.strokeWidth: 5.0, NSAttributedStringKey.strokeColor: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)] as [NSAttributedStringKey : Any]
+        let attributedString = NSAttributedString(string: "Flips: \(flipCount)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
+    }
+    
+    var flipCount = 0 {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
+    
+    var emojiChoices = "🎃👻😈👺😻👅"
+    
+    @IBOutlet private var cardButtons: [UIButton]!
+    
+    @IBOutlet private weak var flipCountLabel: UILabel! {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
+    
+    @IBAction private func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender) {
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+    }
+    
+    private func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            } else {
+                button.setTitle("", for: UIControlState.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            }
+        }
+    }
+    
+    private var emoji = Dictionary<Card, String>()
+    
+    private func emoji(for card: Card) -> String {
+        
+        if emoji[card] == nil {
+            if emojiChoices.count > 0 {
+                let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+                emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
+            }
+        }
+        
+        return emoji[card] ?? "?"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
+
