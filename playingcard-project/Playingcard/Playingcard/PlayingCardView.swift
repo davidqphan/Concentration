0cb1433a34b155812818b8e66242e03b50e281ee
David Phan <david.phan@artamata.com>
2018-09-19 15:34:24 -0700

    added PlayingCardView to story board

---
+++ playingcard-project/Playingcard/Playingcard/PlayingCardView.swift
@@ -0,0 +1,21 @@
+//
+//  PlayingCardView.swift
+//  Playingcard
+//
+//  Created by David Phan on 9/19/18.
+//  Copyright © 2018 David Phan. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
